9d854eb87d290db10bc0627843f8e3f839b58d1f
JaneChoi <n10297545@qut.edu.au>
2020-05-12 21:51:49 +1000

    add landing&nav bar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
 import Footer from './componets/layout/Footer';
+import Navbar from './componets/layout/Navbar';
+import Landing from './componets/layout/Landing';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>stock app</h1>
-     <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+        <Navbar/>
+        <Landing/>
+
+
 
         <Footer/>
     </div>
