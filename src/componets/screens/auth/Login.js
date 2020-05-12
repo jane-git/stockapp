410f0636b92ab44adb59df4f80ee83b9605e395b
JaneChoi <n10297545@qut.edu.au>
2020-05-13 00:18:48 +1000

    Login&Register form

--- src/componets/screens/auth/Login.js
+++ src/componets/screens/auth/Login.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react'
 
-export default class Login extends Component {
+export default class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const newUser = {
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        console.log(newUser);
+    }
+
+
     render() {
+
+        const { email, password, errors } =  this.state;
+
         return (
-            <div>
-                <h1>Login</h1>
+            <div className="register">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 m-auto">
+                        <h1 className="display-4 text-center">Log in</h1>
+                        <p className="lead text-center">
+                            Jane's Stock app Login
+                        </p>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <input
+                                    type="email"
+                                    className="form-control form-control-lg"
+                                    placeholder="Email Address"
+                                    name="email"
+                                    value={email}
+                                    onChange={this.onChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="password"
+                                    className="form-control form-control-lg"
+                                    placeholder="Password"
+                                    name="password"
+                                    value={password}
+                                    onChange={this.onChange}
+                                />
+                            </div>
+                            <input type="submit" className="btn btn-info btn-block mt-4" />
+                        </form>
+                    </div>
+                </div>
             </div>
+        </div>
         )
     }
 }
