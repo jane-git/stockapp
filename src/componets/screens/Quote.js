8f4690edfb9155d8d2d00cdc956c78bb4e17c731
JaneChoi <n10297545@qut.edu.au>
2020-05-12 23:17:41 +1000

    data view from api & spinner

--- src/componets/screens/Quote.js
+++ src/componets/screens/Quote.js
@@ -1,13 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Table } from 'reactstrap';
 
 export default class Quote extends Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Quote
-                </h1>
+            <div className="landing">
+                <div className="row">
+                    <div className="col-md-10 m-auto">
+                        <h1 className="display-4 text-center">Stock Info</h1>
+                        <p className="lead text-center">
+                            {" "}
+                        </p>
+                        <br />
+                        <Table hover>
+                            <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Timestamp</th>
+                                <th>Symbol</th>
+                                <th>Name</th>
+                                <th>Industry</th>
+                                <th>Open</th>
+                                <th>High</th>
+                                <th>Low</th>
+                                <th>Close</th>
+                                <th>Volumes</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>1</th>
+                                    <td>2020-05-13</td>
+                                    <td>Teddy</td>
+                                    <td>Teddy</td>
+                                    <td>Healthcare</td>
+                                    <td>true</td>
+                                    <td>13,000</td>
+                                    <td>1,800</td>
+                                    <td>false</td>
+                                    <td>30,000</td>
+                                    {/*<th scope="row">{index + 1}</th>*/}
+                                    {/*<td>{item.name}</td>*/}
+                                    {/*<td>{item.symbol}</td>*/}
+                                    {/*<td>{item.industry}</td>*/}
+                                </tr>
+                            </tbody>
+
+                        </Table>
+
+                    </div>
+                 </div>
             </div>
+
         )
     }
 }
--- src/componets/screens/Stock.js
+++ src/componets/screens/Stock.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Table } from 'reactstrap';
+import { Table, Spinner } from 'reactstrap';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 
 export default class Stock extends Component {
 
@@ -29,38 +30,58 @@ export default class Stock extends Component {
         //상태값 재선언
         const {loading, results, errors} = this.state;
         console.log(results);
+
+
+
         return (
-            <div className="Landing">
-                <div className="row">
-                    <div className="col-md-10 m-auto">
-                        <h1 className="display-4 text-center">
-                            Stock Info
-                        </h1>
-                        <p className="lead text-center">
-                            {"welcom"}
-                        </p>
-                        <Table hover>
-                            <thead>
-                                <tr>
-                                <th>No</th>
-                                <th>Name</th>
-                                <th>Symbol</th>
-                                <th>Industry</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                            </tbody>
-                        </Table>
+            loading ? (
+                <Spinner color="primary" />
+            ) : (
+                <div className="Landing">
+                    <div className="row">
+                        <div className="col-md-10 m-auto">
+                            <h1 className="display-4 text-center">
+                                Stock Info
+                            </h1>
+                            <p className="lead text-center">
+                                {"welcom"}
+                            </p>
+                            <Table hover>
+                                <thead>
+                                    <tr>
+                                        <th className="text-center" width="10%">No</th>
+                                        <th className="text-center" width="40%">Name</th>
+                                        <th className="text-center" width="20%">Symbol</th>
+                                        <th className="text-center" width="30%">Industry</th>
+                                    </tr>
+                                </thead>
+                                {results.map((item, index) => (
+                                    <tbody>
+                                        <tr>
+                                            <th className="text-center">{index + 1}</th>
+                                            <td className="text-center">{item.name}</td>
+                                            <td className="text-center">
+                                                <Link to='quote'>
+                                                    {item.symbol}
+                                                </Link>
+                                            </td>
+
+                                            <td className="text-center">{item.industry}</td>
+                                        </tr>
+                                    </tbody>
+                                ))}
+
+
+                            </Table>
+                        </div>
+                    
                     </div>
-                
                 </div>
-            </div>
+            )
+
+
+
+
         )
     }
 }
