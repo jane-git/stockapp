1321685cfa008e3d1746ce3aa906264966f2db02
JaneChoi <n10297545@qut.edu.au>
2020-05-20 09:02:25 +1000

    navi menu changed

--- src/componets/layout/Navbar.js
+++ src/componets/layout/Navbar.js
@@ -1,107 +1,86 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { logoutUser } from '../../actions/authActions';
-
+/* eslint-disable react-hooks/exhaustive-deps, jsx-a11y/anchor-is-valid*/
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 
 class Navbar extends Component {
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser(this.props.history);
+  }
 
-    onLogoutClick(e) {
-        e.preventDefault();
-        this.props.logoutUser();
-    }
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
 
-    render() {
-        
-        const { isAuthenticated, user } = this.props.auth;
+    const authLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/stock">
+            Stocks
+          </Link>
+        </li>
+        <a href="" onClick={this.onLogoutClick.bind(this)} className="nav-link">
+          {user.email}
+        </a>
+        <a href="" onClick={this.onLogoutClick.bind(this)} className="nav-link">
+        {"Logout"}
+      </a>
+      </ul>
+    );
 
-        const authLinks = (
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <Link className="nav-link" to="stock">
-                        Stock
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="quote">
-                        Quote
-                    </Link>
-                </li>
-                <a 
-                    href="" 
-                    onClick={this.onLogoutClick.bind(this)}
-                    className="nav-link"
-                >
-                    {user.email}
-                    
-                </a>
-             </ul>
-            
-        );
-        
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/stock">
+            Stocks
+          </Link>
+        </li>        
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Sign up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Log in
+          </Link>
+        </li>
+      </ul>
+    );
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Jane's Stock one - Main
+          </Link>
 
-        const guestLinks = (
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <Link className="nav-link" to="stock">
-                        Stock
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="quote">
-                        Quote
-                    </Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link" to="register">
-                    Sign Up
-                </Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="login">
-                        Login
-                    </Link>
-                </li>
-            </ul>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
 
-        )
-        return (
-           <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-                <div className="container">
-                    <Link className="navbar-brand" to="/">
-                        Jane's Stock one - Main
-                    </Link>
-   
-                    <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#mobile-nav"
-                >
-                    <span className="navbar-toggler-icon" />
-                </button>
-
-
-                    <div className="collapse navbar-collapse" id="mobile-nav">
-
-
-                    {isAuthenticated ? authLinks : guestLinks}
- 
-                    </div>
-                </div>
-           </nav>
-        )
-    }
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            {isAuthenticated ? authLinks : guestLinks}
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 Navbar.propTypes = {
-    logoutUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-    auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
-export default connect(mapStateToProps, {logoutUser})(Navbar);
+export default connect(mapStateToProps, { logoutUser })(withRouter(Navbar));
