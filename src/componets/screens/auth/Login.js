96159e2a1caf480b59f06b05877f143e2e6f9956
JaneChoi <n10297545@qut.edu.au>
2020-05-20 09:04:44 +1000

    Register&Login complete

--- src/componets/screens/auth/Login.js
+++ src/componets/screens/auth/Login.js
@@ -1,111 +1,113 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 // import axios from 'axios';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { loginUser } from '../../../actions/authActions';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../../actions/authActions";
 
- class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            errors: {}
-        };
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
 
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
     }
+  }
 
-    componentDidMount() {
-        if (this.props.auth.isAuthenticated) {
-            this.props.history.push('/dashboard');
-        }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.auth.isAuthenticated) {
-            this.props.history.push('/dashboard');
-        }
-    }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
+  onSubmit(e) {
+    e.preventDefault();
 
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
 
-    onChange(e) {
-        this.setState({[e.target.name]: e.target.value});
-    }
+    // console.log(logInUser);
+    // axios
+    //     .post("http://131.181.190.87:3000/user/login", logInUser)
+    //     .then(res => console.log(res.data))
+    //     .catch(err => console.log(err.message));
+    this.props.loginUser(
+      userData,
+      this.props.history,
+      this.props.location.state ? this.props.location.state.successUrl : null
+    );
+  }
 
-    onSubmit(e) {
-        e.preventDefault();
+  render() {
+    const { email, password } = this.state;
 
-        const userData = {
-            email: this.state.email,
-            password: this.state.password
-        };
-
-        // console.log(logInUser);
-        // axios
-        //     .post("http://131.181.190.87:3000/user/login", logInUser)
-        //     .then(res => console.log(res.data))
-        //     .catch(err => console.log(err.message));
-        this.props.loginUser(userData);
-    }
-
-
-    render() {
-
-        const { email, password, errors } =  this.state;
-
-        return (
-            <div className="login">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 m-auto">
-                        <h1 className="display-4 text-center">Log in</h1>
-                        <p className="lead text-center">
-                            Jane's Stock one Login
-                        </p>
-                        <form onSubmit={this.onSubmit}>
-                            <div className="form-group">
-                                <input
-                                    type="email"
-                                    className="form-control form-control-lg"
-                                    placeholder="Email Address"
-                                    name="email"
-                                    value={email}
-                                    onChange={this.onChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <input
-                                    type="password"
-                                    className="form-control form-control-lg"
-                                    placeholder="Password"
-                                    name="password"
-                                    value={password}
-                                    onChange={this.onChange}
-                                />
-                            </div>
-                            <input type="submit" className="btn btn-info btn-block mt-4" />
-                        </form>
-                    </div>
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log in</h1>
+              <p className="lead text-center">Jane's Stock one Login</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                  />
                 </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  className="btn btn-info btn-block mt-4"
+                  value="Log in"
+                />
+              </form>
             </div>
+          </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 Login.propTypes = {
-    loginUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    errors: PropTypes.object.isRequired
-
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors,
 });
 
-export default connect(mapStateToProps, {loginUser})(Login);
+export default connect(mapStateToProps, { loginUser })(withRouter(Login));
--- src/componets/screens/auth/Register.js
+++ src/componets/screens/auth/Register.js
@@ -1,110 +1,106 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { registerUser } from '../../../actions/authActions';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { registerUser } from "../../../actions/authActions";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
 
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            errors: {}
-        };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
     }
+  }
 
-    componentDidMount() {
-        if (this.props.auth.isAuthenticated) {
-            this.props.history.push('/dashboard');
-        }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
-    
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.errors) {
-            this.setState({ errors: nextProps.errors});
-        }
-    }
+  }
 
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    onChange(e) {
-        this.setState({[e.target.name]: e.target.value});
-    }
+  onSubmit(e) {
+    e.preventDefault();
 
-    onSubmit(e) {
-        e.preventDefault();
+    const newUser = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    // axios
+    //     .post("http://131.181.190.87:3000/user/register", newUser)
+    //     .then(res => console.log(res.data))
+    //     .catch(err => console.log(err.message));
+    this.props.registerUser(newUser, this.props.history);
+  }
 
-        const newUser = {
-            email: this.state.email,
-            password: this.state.password
-        };
-        // axios
-        //     .post("http://131.181.190.87:3000/user/register", newUser)
-        //     .then(res => console.log(res.data))
-        //     .catch(err => console.log(err.message));
-        this.props.registerUser(newUser, this.props.history);
-    }
+  render() {
+    const { email, password } = this.state;
 
-
-    render() {
-
-        const { email, password, errors } =  this.state;
-
-        return (
-            <div className="register">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 m-auto">
-                        <h1 className="display-4 text-center">Sign Up</h1>
-                        <p className="lead text-center">
-                            Jane's Stock app Register
-                        </p>
-                        <form onSubmit={this.onSubmit}>
-                            <div className="form-group">
-                                <input
-                                    type="email"
-                                    className="form-control form-control-lg"
-                                    placeholder="Email Address"
-                                    name="email"
-                                    value={email}
-                                    onChange={this.onChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <input
-                                    type="password"
-                                    className="form-control form-control-lg"
-                                    placeholder="Password"
-                                    name="password"
-                                    value={password}
-                                    onChange={this.onChange}
-                                />
-                            </div>
-                            <input type="submit" className="btn btn-info btn-block mt-4" />
-                        </form>
-                    </div>
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign up</h1>
+              <p className="lead text-center">Jane's Stock one Register</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                  />
                 </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  className="btn btn-info btn-block mt-4"
+                  value="Sign up"
+                />
+              </form>
             </div>
+          </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 Register.propTypes = {
-    registerUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    errors: PropTypes.object.isRequired
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors,
 });
-
 
 export default connect(mapStateToProps, { registerUser })(withRouter(Register));
