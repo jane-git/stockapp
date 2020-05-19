585bbb39343c5aaf9be4e7ec151955383f9a3f67
JaneChoi <n10297545@qut.edu.au>
2020-05-20 09:07:52 +1000

    can get data from api(3) & add chart

--- src/componets/screens/Quote/QuoteContainer.js
+++ src/componets/screens/Quote/QuoteContainer.js
@@ -1,44 +1,159 @@
-import React, { Component } from 'react';
-import { Table } from 'reactstrap';
-import { baseAPIs } from '../../../API';
-import QuotePrensenter from './QuotePresenter';
+import React, { useEffect, useCallback, useState } from "react";
+import { connect } from "react-redux";
+import moment from "moment";
+import { baseAPIs } from "../../../API";
+import QuotePrensenter from "./QuotePresenter";
 
-export default class QuoteContainer extends Component {
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
 
-    //하나의 객체만 받는다.
-    constructor() {
-        super();
-        this.state = {
-            loading: true,
-            result: {},
-            errors: {}
-        };
+export default connect(mapStateToProps)((props) => {
+  const {
+    match: {
+      params: { symbol },
+    },
+    auth: { isAuthenticated },
+  } = props;
+
+  const [state, setState] = useState({
+    loading: true,
+    results: null,
+    errors: {},
+  });
+  const [initTable, setInitTable] = useState(false);
+  const [dataTable, setDataTable] = useState(null);
+
+  const fetchData = useCallback(async () => {
+    let results = [];
+    try {
+      const { data } = await baseAPIs.quoteAPI(symbol);
+      results = [data];
+    } catch {
+      setState({ error: "Cant't find anything." });
+    } finally {
+      setState({ loading: false, results });
+      setInitTable(true);
     }
+  }, [symbol]);
 
-    async componentDidMount() {
-        const { match: {
-            params: { symbol }
-        }, history: { push }
-        } = this.props;
-        
-        try {
-            const { data } = await baseAPIs.quoteAPI(symbol);
-            this.setState({result: data});
-        } catch {
-            this.setState({ error: "Cant't find anything."});
-        } finally {
-            this.setState({ loading: false});
-        }    
+  const handleDrawChart = useCallback((results) => {
+    let labels = [];
+    let data = [];
+    for (const item of results) {
+      labels.push(moment(item.timestamp).format("DD/MM/YYYY"));
+      data.push(item.close);
     }
+    const ctx = document.getElementById("chart").getContext("2d");
+    new window.Chart(ctx, {
+      // Select chart type
+      type: "line",
+      // Data of Chart
+      data: {
+        labels: labels.reverse(),
+        datasets: [
+          {
+            label: "Closing Price",
+            backgroundColor: "transparent",
+            borderColor: "#64bbad",
+            data: data.reverse(),
+          },
+        ],
+      },
+      // Option
+      options: {
+        legend: {
+          display: false,
+        },
+      },
+    });
+  }, []);
 
-    render() {
-        const {result, loading, error} = this.state;
-        return (
-            <QuotePrensenter
-                loading={loading}
-                result={result}
-                error={error}
-            />
-        )
+  const handleClickSearch = useCallback(async () => {
+    dataTable.destroy();
+    setInitTable(false);
+    const sDate = moment(
+      document.getElementById("sDate").value,
+      "DD/MM/YYYY"
+    ).toISOString();
+    const eDate = moment(
+      document.getElementById("eDate").value,
+      "DD/MM/YYYY"
+    ).toISOString();
+
+    let results = [];
+    try {
+      const { data } = await baseAPIs.priceHistoryAPI(symbol, sDate, eDate);
+      results = data;
+    } catch {
+      setState({ error: "Cant't find anything." });
+    } finally {
+      console.log(results);
+      setTimeout(() => {
+        setState({
+          ...state,
+          results,
+        });
+        setInitTable(true);
+        if(results){
+          handleDrawChart(results);
+        }        
+      }, 100);
     }
-}
+  }, [dataTable, handleDrawChart, state, symbol]);
+
+  const handleGoToLogin = useCallback(() => {
+    props.history.push({
+      pathname: "/login",
+      state: { successUrl: `/quote/${symbol}` },
+    });
+  }, [props.history, symbol]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  useEffect(() => {
+    if (!state.loading && state.results && state.results.length) {
+      window.$("#sDate").datepicker({
+        uiLibrary: "bootstrap4",
+        format: "dd/mm/yyyy",
+        value: moment(state.results[0].timestamp)
+          .subtract(1, "days")
+          .format("DD/MM/YYYY"),
+      });
+      window.$("#eDate").datepicker({
+        uiLibrary: "bootstrap4",
+        format: "dd/mm/yyyy",
+        value: moment(state.results[0].timestamp).format("DD/MM/YYYY"),
+        disableDates: function (date) {
+          const sDate = moment(
+            document.getElementById("sDate").value,
+            "DD/MM/YYYY"
+          );
+          return sDate < date ? true : false;
+        },
+      });
+    }
+  }, [state.loading, state.results]);
+  useEffect(() => {
+    if (initTable) {
+      const dataTable = window.$("#quote_table").DataTable({
+        bInfo: false,
+        bLengthChange: false,
+        pageLength: 6,
+        sDom: "lrtip",
+      });
+      setDataTable(dataTable);
+    }
+  }, [initTable]);
+  return (
+    <QuotePrensenter
+      {...state}
+      initTable={initTable}
+      handleClickSearch={handleClickSearch}
+      isAuthenticated={isAuthenticated}
+      handleGoToLogin={handleGoToLogin}
+    />
+  );
+});
--- src/componets/screens/Quote/QuotePresenter.js
+++ src/componets/screens/Quote/QuotePresenter.js
@@ -1,67 +1,113 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Table, Spinner } from 'reactstrap';
+import React from "react";
+import PropTypes from "prop-types";
+import { Table, Spinner, Button } from "reactstrap";
+import moment from "moment";
 
-const QuotePresenter = ({ result, error, loading }) => (
-    loading ? (
-        <Spinner color="primary"/>
-    ) : (
-        <div className="Landing">
-            <div className="row">
-                <div className="col-md-10 m-auto">
-                    <h1 className="display-4 text-center">
-                        Quote Info
-                    </h1>
-                        <p className="lead text-center" >
-                            {"Quote to get the latest price information by stock symbol"}
-                        </p>
-                        <br />
-                        <Table hover>
-                            <thead>
-                            <tr>
-                                <th>Timestamp</th>
-                                <th>Symbol</th>
-                                <th>Name</th>
-                                <th>Industry</th>
-                                <th>Open</th>
-                                <th>High</th>
-                                <th>Low</th>
-                                <th>Close</th>
-                                <th>Volumes</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{result.timestamp}</td>
-                                    <td>{result.symbol}</td>
-                                    <td>{result.name}</td>
-                                    <td>{result.industry}</td>
-                                    <td>{result.open}</td>
-                                    <td>{result.high}</td>
-                                    <td>{result.low}</td>
-                                    <td>{result.close}</td>
-                                    <td>{result.volumes}</td>
-                                    {/*<th scope="row">{index + 1}</th>*/}
-                                    {/*<td>{item.name}</td>*/}
-                                    {/*<td>{item.symbol}</td>*/}
-                                    {/*<td>{item.industry}</td>*/}
-                                </tr>
-                            </tbody>
-                        </Table>
-                 </div>
-            </div>
+const QuotePresenter = ({
+  loading,
+  results,
+  initTable,
+  handleClickSearch,
+  isAuthenticated,
+  handleGoToLogin,
+}) =>
+  loading ? (
+    <Spinner color="primary" />
+  ) : (
+    <div className="Landing">
+      <div className="row">
+        <div className="col-md-10 m-auto">
+          <h1 className="display-4 text-center">Quote & Price History</h1>
+          <br></br> 
+          <p className="lead text-center">
+            {"The first date set is 'Quote' information."} <br></br> 
+            {"'Price History' : Choose a date."}<br></br> 
+          </p>
+          <br></br> 
+          {results && results[0] && (
+            <p className="lead text-center">
+              {results[0].symbol} - {results[0].name}
+            </p>
+          )}
+
+          <div className="quote-search-wrapper">
+            {isAuthenticated ? (
+              <>
+                <span className="mr">From</span>
+                <input id="sDate" />
+                <span className="ml mr">To</span>
+                <input id="eDate" />
+                <Button
+                  className="ml"
+                  color="primary"
+                  onClick={handleClickSearch}
+                >
+                  Search
+                </Button>
+              </>
+            ) : (
+              <Button
+                color="warning"
+                className="text-white"
+                onClick={handleGoToLogin}
+              >
+                Login to access stock history
+              </Button>
+            )}
+          </div>
+          {initTable ? (
+            <>
+              <Table id="quote_table" hover>
+                <thead>
+                  <tr>
+                    <th>Timestamp</th>
+                    <th>Symbol</th>
+                    <th>Name</th>
+                    <th>Industry</th>
+                    <th>Open</th>
+                    <th>High</th>
+                    <th>Low</th>
+                    <th>Close</th>
+                    <th>Volumes</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {results.map((item, idx) => (
+                    <tr key={`quote_${idx}`}>
+                      <td>{moment(item.timestamp).format("DD/MM/YYYY")}</td>
+                      <td>{item.symbol}</td>
+                      <td>{item.name}</td>
+                      <td>{item.industry}</td>
+                      <td>{item.open}</td>
+                      <td>{item.high}</td>
+                      <td>{item.low}</td>
+                      <td>{item.close}</td>
+                      <td>{item.volumes}</td>
+                      {/*<th scope="row">{index + 1}</th>*/}
+                      {/*<td>{item.name}</td>*/}
+                      {/*<td>{item.symbol}</td>*/}
+                      {/*<td>{item.industry}</td>*/}
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+              <div className="chart-wrapper">
+                <canvas id="chart"></canvas>
+              </div>
+            </>
+          ) : null}
         </div>
-
-    )
- 
-)
-
+      </div>
+    </div>
+  );
 
 QuotePresenter.propTypes = {
-    result: PropTypes.object,
-    error: PropTypes.string,
-    loading: PropTypes.bool.isRequired    
-    
+  loading: PropTypes.bool,
+  results: PropTypes.array,
+  initTable: PropTypes.bool,
+  handleClickSearch: PropTypes.func,
+  isAuthenticated: PropTypes.bool,
+  handleGoToLogin: PropTypes.func,
 };
 
 export default QuotePresenter;
